c2a5b7264af96d299eff973b53a7b0643c2c3ab3
beepscore <support@beepscore.com>
2014-11-25 00:41:14 -0800

    In build.gradle dependencies add com.android.support:support-v4:20.0.0
    Add SingleFragmentActivity, NerdLauncherFragment, and activity_fragment.xml.
    In NerdLauncherActivity extend SingleFragmentActivity.
    In NerdLauncherActivity delete override onCreate, onCreateOptionsMenu, onOptionsItemSelected. SingleFragmentActivity overrides onCreate.

---
+++ app/src/main/java/com/beepscore/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,9 @@
+package com.beepscore.android.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by stevebaker on 11/25/14.
+ */
+public class NerdLauncherFragment extends Fragment {
+}
