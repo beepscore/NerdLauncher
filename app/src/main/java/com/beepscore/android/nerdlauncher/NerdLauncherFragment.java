770150c249c1ac4aabd7bcf192dd996b9539f045
beepscore <support@beepscore.com>
2014-11-26 17:08:51 -0800

    In NerdLauncherFragment extend change Fragment to ListFragment. Add method sortActivities. Instantiate an adapter and setListAdapter. Now app shows list of apps.

--- app/src/main/java/com/beepscore/android/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/beepscore/android/nerdlauncher/NerdLauncherFragment.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created by stevebaker on 11/25/14.
  */
-public class NerdLauncherFragment extends Fragment {
+public class NerdLauncherFragment extends ListFragment {
     public static final String TAG = "NerdLauncherFragment";
 
     @Override
@@ -31,5 +38,36 @@ public class NerdLauncherFragment extends Fragment {
         List<ResolveInfo> activities = packageManager.queryIntentActivities(startupIntent, 0);
 
         Log.d(TAG, "I've found " + Integer.toString(activities.size()) + " activities");
+
+        sortActivities(activities);
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(
+                getActivity(), android.R.layout.simple_list_item_1, activities) {
+
+            public View getView(int position, View convertView, ViewGroup parent) {
+                PackageManager packageManager = getActivity().getPackageManager();
+                View view = super.getView(position, convertView, parent);
+                // view will be a simple_list_item_1, a TextView.
+                // Cast in order to set its text
+                TextView textView = (TextView)view;
+                ResolveInfo resolveInfo = getItem(position);
+                textView.setText(resolveInfo.loadLabel(packageManager));
+                return view;
+            }
+        };
+
+        setListAdapter(adapter);
     }
+
+    private void sortActivities(List<ResolveInfo> activities) {
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            public int compare(ResolveInfo resolveInfoA, ResolveInfo resolveInfoB) {
+                PackageManager packageManager = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        resolveInfoA.loadLabel(packageManager).toString(),
+                        resolveInfoB.loadLabel(packageManager).toString());
+            }
+        });
+    }
+
 }
